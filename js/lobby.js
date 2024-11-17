c29a3eacdc540a416986ecf3e8854ab06cd0277d
iKuta <ardandor120306@gamil.com>
2024-11-17 23:55:13 +0800

    bug fix

--- js/lobby.js
+++ js/lobby.js
@@ -5,7 +5,17 @@ if(displayName){
     form.name.value = displayName
     
 }
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
+    sessionStorage.setItem('display_name', e.target.name.value)
+
+    let inviteCode = e.target.room.value
+    if(!inviteCode){
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    }
+    window.location = `room.html?room=${inviteCode}`
+})
 
     
 
