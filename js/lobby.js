c8a1aff8614f6c00fbedbb8183a8d6ba8eb812a9
iKuta <ardandor120306@gamil.com>
2024-11-17 21:42:03 +0800

    update readme

--- js/lobby.js
+++ js/lobby.js
@@ -6,17 +6,9 @@ if(displayName){
     
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
 
-    sessionStorage.setItem('display_name', e.target.name.value)
+    
 
-    let inviteCode = e.target.room.value
-    if(!inviteCode){
-        inviteCode = String(Math.floor(Math.random() * 10000))
-    }
-    window.location = `room.html?room=${inviteCode}`
-})
 
 // Статический список комнат с id и name
 const rooms = [
@@ -77,9 +69,8 @@ form.addEventListener('submit', (e) => {
         inviteCode = String(Math.floor(Math.random() * 10000));
     }
     
-    // Добавление новой комнаты в список с уникальным id
     const newRoom = {
-        id: rooms. e.target.room.value, // Уникальный id для новой комнаты
+        id: rooms. e.target.room.value, 
         name: inviteCode
     };
     rooms.push(newRoom);
